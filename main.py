8c01c0db7930267614ff518d6fa8f46439e65198
Mary_Ross <amoscomaryross@gmail.com>
2026-02-05 09:05:06 +0800

    Enable login for Amosco Branch

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print("Lab1:Version Control")
-print("Author: AMOSCO MARY ROSS") 
-print ("Student ID: TUPM-25-1206")
-print(Class Section: BSECE-1B) #Add Section
+SYSTEM ERROR: CRITICAL FAILURE
+print ("login Feature Enabled")
+print(Login Feature Enabled")
